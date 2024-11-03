931a229a6603d93e722df73cfbf81c396085113a
Michał Kuliga <mkuliga72@gmail.com>
2024-11-04 00:02:04 +0100

    Initial commit

---
+++ HairSaloon.Models/Appointment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HairSaloon.Models;
+
+public class Appointment
+{
+    public int Id { get; set; }
+    public int ServiceId { get; set; }
+    [ForeignKey("ServiceId")]
+    public Service Service { get; set; }
+    public int UserId { get; set; }
+    [ForeignKey("UserId")]
+    public User User { get; set; }
+    public DateTime Date { get; set; }
+}
