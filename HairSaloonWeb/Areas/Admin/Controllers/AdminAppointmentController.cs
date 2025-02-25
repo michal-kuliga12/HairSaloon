e1a0a20fb5187968377514acb426620e39b73e63
Michał Kuliga <mkuliga72@gmail.com>
2025-02-26 00:50:03 +0100

    Appointment table in admin dashboard
    - Added appointment list generation,
    - delete appointment functionality
    - fixes in UI: admin layout, blogs

---
+++ HairSaloonWeb/Areas/Admin/Controllers/AdminAppointmentController.cs
@@ -0,0 +1,56 @@
+﻿using HairSaloon.DataAccess.Data;
+using HairSaloon.DataAccess.Repository.IRepository;
+using HairSaloon.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HairSaloonWeb.Areas.Customer.Controllers;
+
+[Area("Admin")]
+public class AdminAppointmentController : Controller
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    private readonly ApplicationDbContext _db;
+
+    public AdminAppointmentController(IUnitOfWork unitOfWork, UserManager<ApplicationUser> userManager, ApplicationDbContext db)
+    {
+        _unitOfWork = unitOfWork;
+        _userManager = userManager;
+        _db = db;
+    }
+
+    public IActionResult Index()
+    {
+        var appointments = _unitOfWork.Appointments.GetAll();
+        return View(appointments);
+    }
+
+
+    #region API CALLS
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        List<Appointment> appointmentList = _unitOfWork.Appointments.GetAll(includeProperties: "Employee,Service").ToList();
+        return Json(new { data = appointmentList });
+    }
+
+    [HttpDelete, ActionName("Delete")]
+    public IActionResult Delete(int id)
+    {
+        Appointment? appointmentFromDb = _unitOfWork.Appointments.Get(s => s.Id == id);
+
+        if (appointmentFromDb == null)
+            return Json(new { success = false, message = "Wystąpił problem usuwania" });
+
+        _unitOfWork.Appointments.Remove(appointmentFromDb);
+        _unitOfWork.Save();
+
+        return Json(new { success = true, message = "Pomyślnie usunięto element" });
+    }
+
+    #endregion
+}
+
---
+++ HairSaloonWeb/wwwroot/js/appointment.js
@@ -0,0 +1,56 @@
+﻿$(document).ready(() => {
+    loadDataTable();
+})
+
+const loadDataTable = () => {
+    dataTable = $('#tblData').DataTable({
+        "responsive": true,
+        "ajax": { url: "/admin/adminappointment/getall" },
+        "columns": [
+            { data: 'service.name', "width": "15%" },
+            {
+                data: 'service.price',
+                "width": "10%",
+                "render": (data) => {
+                    return `${data} zł`
+                }
+            },
+            { data: 'customerFirstName', "width": "10%" },
+            { data: 'customerEmail', "width": "10%" },
+            { data: 'customerPhoneNumber', "width": "10%" },
+            {
+                data: null,
+                "width": "10%",
+                "render": (row) => {
+                    return `${row.employee.firstName} ${row.employee.lastName}`
+                }
+            },
+            {
+                data: 'id',
+                width: "30%",
+                "render": (data) => {
+                    return `<div class="btn-group w-100 gap-2">
+                        <button onClick=Update('/Admin/AdminAppointment/Update?id=${data}') class="d-flex gap-1 w-50 justify-content-center align-items-center btn btn-dark"><i class="bi bi-pen"> Edytuj</i></button>
+                        <button onClick=Delete('/Admin/AdminAppointment/Delete?id=${data}') class="d-flex gap-1 w-50 justify-content-center align-items-center btn btn-danger"><i class="bi bi-trash"> Usuń</i></button>
+                    </div>`
+                }
+            }
+        ]
+    })
+    dataTable.ajax.reload();
+}
+
+const Delete = (url) => {
+    $.ajax({
+        url: url,
+        type: 'DELETE',
+        success: (data) => {
+            dataTable.ajax.reload();
+        }
+    }) 
+}
+
+const Update = (url) => {
+    window.location.href = url;
+}
+
